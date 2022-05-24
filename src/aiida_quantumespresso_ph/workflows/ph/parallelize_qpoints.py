cd738fc2fe3a1d46eaddee0498a1e4a69792e623
Sebastiaan Huber <mail@sphuber.net>
2022-05-24 16:48:53 +0200

    Add the `PhParallelizeQpointsWorkChain` work chain
    This work chain was originally developed in `aiida-quantumespresso-epfl`.
    It automatically parallelizes the work by running an individual
    `PhBaseWorkChain` for each q-point in the mesh. First it launches an
    initialization `PhBaseWorkChain` to determine the explicit q-points.
    Then they are distributed into individual `KpointsData` nodes using the
    `distribute_qpoints` calcfunction. For each q-point an individual
    `PhBaseWorkChain` is launched and in the end the dynamical matrices
    computed by each are collected in a single `FolderData` output node
    through the `recollect_qpoints` calcfunction.
    
    The `PhWorkChain` is a simple wrapper around the parallelization work
    chain that allows toggling the parallelization with a simple boolean
    `parallelize_qpoints` input.

---
+++ src/aiida_quantumespresso_ph/workflows/ph/parallelize_qpoints.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+"""Workchain to perform a ``PhBaseWorkChain`` with automatic parallelization over q-points."""
+from aiida.engine import WorkChain, append_
+from aiida.orm import Bool, FolderData
+from aiida.plugins import CalculationFactory, WorkflowFactory
+
+PhBaseWorkChain = WorkflowFactory('quantumespresso.ph.base')
+distribute_qpoints = CalculationFactory('quantumespresso_ph.distribute_qpoints')
+recollect_qpoints = CalculationFactory('quantumespresso_ph.recollect_qpoints')
+
+
+class PhParallelizeQpointsWorkChain(WorkChain):
+    """Workchain to perform a ``PhBaseWorkChain`` with automatic parallelization over q-points.
+
+    This workchain differs from the ``PhBaseWorkChain`` in that the computation is parallelized over the q-points. For
+    each individual q-point a separate ``PhBaseWorkChain`` is run. At the end, the computed dynamical matrices of each
+    individual workchain are collected into a single ``FolderData`` as output.
+    """
+
+    @classmethod
+    def define(cls, spec):
+        """Define the process specification."""
+        super().define(spec)
+        spec.expose_inputs(PhBaseWorkChain, exclude=('only_initialization',))
+        spec.outline(
+            cls.run_ph_init,
+            cls.run_distribute_qpoints,
+            cls.run_ph_qgrid,
+            cls.run_recollect_qpoints,
+            cls.results,
+        )
+        spec.output('retrieved', valid_type=FolderData)
+
+    def run_ph_init(self):
+        """Run a first dummy ``PhBaseWorkChain`` that will exit straight after initialization.
+
+        At that point it will have generated the q-point list, which we use to determine how to distribute these over
+        the available computational resources.
+        """
+        inputs = self.exposed_inputs(PhBaseWorkChain)
+
+        # Toggle the only initialization flag and define minimal resources
+        inputs['only_initialization'] = Bool(True)
+        inputs['ph']['metadata']['options']['max_wallclock_seconds'] = 1800
+
+        node = self.submit(PhBaseWorkChain, **inputs)
+        self.report(f'launching initialization PhBaseWorkChain<{node.pk}>')
+        self.to_context(ph_init=node)
+
+    def run_distribute_qpoints(self):
+        """Distribute the q-points."""
+        self.report('launching `distribute_qpoints`')
+        retrieved = self.ctx.ph_init.outputs.retrieved
+        self.ctx.qpoints = distribute_qpoints(retrieved=retrieved)
+
+    def run_ph_qgrid(self):
+        """Launch individual ``PhBaseWorkChain``s for each distributed q-point."""
+        inputs = self.exposed_inputs(PhBaseWorkChain)
+
+        for _, qpoint in sorted(self.ctx.qpoints.items()):
+            inputs['ph']['qpoints'] = qpoint
+            node = self.submit(PhBaseWorkChain, **inputs)
+            self.report(f'launching PhBaseWorkChain<{node.pk}> for q-point<{qpoint.pk}>')
+            self.to_context(workchains=append_(node))
+
+    def run_recollect_qpoints(self):
+        """Recollect the dynamical matrices from individual q-points calculations."""
+        self.report('launching `recollect_qpoints`')
+        retrieved_folders = {'qpoint_0': self.ctx.ph_init.outputs.retrieved}
+
+        for index, workchain in enumerate(self.ctx.workchains):
+            retrieved_folders[f'qpoint_{index}'] = workchain.outputs.retrieved
+
+        self.ctx.merged_retrieved = recollect_qpoints(**retrieved_folders)
+
+    def results(self):
+        """Attach the ``FolderData`` with all collected dynamical matrices as output."""
+        self.out('retrieved', self.ctx.merged_retrieved)
+        self.report('workchain completed successfully')
