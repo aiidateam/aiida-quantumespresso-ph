f05f3a56e617fbe0460908341307522b8b427a8c
Lorenzo <79980269+bastonero@users.noreply.github.com>
2025-12-19 16:38:04 +0000

    use NaCl for test

--- examples/test_dynamical_workchain.py
+++ examples/test_dynamical_workchain.py
@@ -4,10 +4,9 @@
 from aiida import load_profile
 from aiida.engine import run
 from aiida.orm import StructureData, load_code
-from aiida.tools.data.structure import spglib_tuple_to_structure, structure_to_spglib_tuple
 from aiida_quantumespresso.common.types import ElectronicType, RelaxType
-from ase.build import bulk
-from spglib import spglib
+from ase.atoms import Atoms
+import numpy as np
 
 from aiida_quantumespresso_ph.workflows.dynamical_matrix import DynamicalMatrixWorkChain
 
@@ -16,25 +15,51 @@ load_profile()
 
 def test_dynamical_matrix():
     """Run a simple example of the dynamical matrix workflow."""
-    structure = StructureData(ase=bulk('Si', a=5.43, cubic=True))
-    cell, _, _ = structure_to_spglib_tuple(structure)
-    structure = spglib_tuple_to_structure(spglib.find_primitive(cell, symprec=1e-5))
+    # Lattice parameter for NaCl (Angstrom)
+    alat = 5.64
+
+    # Define primitive cell vectors (in Angstrom)
+    cell = alat * np.array([[0.0, 0.5, 0.5], [0.5, 0.0, 0.5], [0.5, 0.5, 0.0]])
+
+    # Fractional (scaled) positions of basis atoms in the primitive cell
+    positions = [
+        (0.0, 0.0, 0.0),  # Na
+        (0.5, 0.5, 0.5)  # Cl
+    ]
+
+    # Corresponding atomic symbols
+    symbols = ['Na', 'Cl']
+
+    # Create the primitive NaCl structure
+    nacl_primitive = Atoms(symbols=symbols, scaled_positions=positions, cell=cell, pbc=True)
+
+    structure = StructureData(ase=nacl_primitive)
 
     pw_code = load_code('pw@localhost')
     ph_code = load_code('ph@localhost')
     overrides = {
-        'clean_workdir': False,
+        'clean_workdir': True,
         'relax': {
             'base': {
                 'kpoints_distance': 0.6,
-            }
+                'pw': {
+                    'parameters': {
+                        'SYSTEM': {
+                            'ecutwfc': 40,
+                            'ecutrho': 40 * 8,
+                        },
+                    },
+                },
+            },
         },
         'ph_main': {
-            'kpoints_distance': 1.2,
+            'parallelize_qpoints': False,
+            'qpoints_distance': 1.2,
             'ph': {
                 'parameters': {
                     'INPUTPH': {
                         'epsil': True,
+                        'tr2_ph': 1.0e-12,
                     }
                 }
             }
@@ -53,11 +78,16 @@ def test_dynamical_matrix():
         }
     )
 
-    _, node = run.get_node(builder)
+    results, node = run.get_node(builder)
     assert node.is_finished_ok, f'{node} failed: [{node.exit_status}] {node.exit_message}'
-    # diff = abs(node.outputs. ...)
-    # print('Max discrepancy is: ', diff)
-    # assert diff <= 1.0e-2
+
+    diff = abs(results['ph_output_parameters'].dict.dielectric_constant[0][0] - 2.60)
+    print('Max discrepancy is: ', diff)
+    assert diff <= 1.0e-1
+
+    diff = abs(results['ph_output_parameters'].dict.effective_charges_eu[0][0][0] - 1.11)
+    print('Max discrepancy is: ', diff)
+    assert diff <= 1.0e-1
 
 
 if __name__ == '__main__':
